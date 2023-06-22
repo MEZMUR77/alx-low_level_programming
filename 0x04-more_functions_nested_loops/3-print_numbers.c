be354bab277fe2397984456b63d6ea5c54169dfb
MEZMUR77 <mezmurdawit44@gmail.com>
2023-06-22 10:55:53 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+ #include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
